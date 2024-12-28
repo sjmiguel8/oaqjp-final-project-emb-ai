a27d07297f14a3ae2aa1b8bb0e152e90ef6e5dae
SJMiguel8 <bonilla.miguel.e@gmail.com>
2024-12-27 22:53:39 -0500

    deployment2

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,6 +2,16 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
+    if not text_to_analyse:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
--- server.py
+++ server.py
@@ -3,27 +3,45 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route('/emotionDetector', methods=['GET', 'POST'])
+@app.route('/emotionDetector', methods=['POST'])
 def detect_emotion():
-    if request.method == 'POST':
-        data = request.get_json()
-        text_to_analyze = data.get('text')
-        
-        if not text_to_analyze:
-            return jsonify({"error": "Invalid input! Try again."}), 400
-        
-        result = emotion_detector(text_to_analyze)
+    data = request.get_json()
+    text_to_analyze = data.get('text')
+    
+    if not text_to_analyze:
         response = {
-            "anger": result.get('anger', 0),
-            "disgust": result.get('disgust', 0),
-            "fear": result.get('fear', 0),
-            "joy": result.get('joy', 0),
-            "sadness": result.get('sadness', 0),
-            "dominant_emotion": result.get('dominant_emotion', '')
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None,
+            "message": "Invalid text! Please try again!"
         }
-        return jsonify(response), 200
-    else:
-        return jsonify({"message": "Use POST method to analyze emotions."}), 200
+        return jsonify(response), 400
+    
+    result = emotion_detector(text_to_analyze)
+    if result.get('dominant_emotion') is None:
+        response = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None,
+            "message": "Invalid text! Please try again."
+        }
+        return jsonify(response), 400
+    
+    response = {
+        "anger": result.get('anger', 0),
+        "disgust": result.get('disgust', 0),
+        "fear": result.get('fear', 0),
+        "joy": result.get('joy', 0),
+        "sadness": result.get('sadness', 0),
+        "dominant_emotion": result.get('dominant_emotion', '')
+    }
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
