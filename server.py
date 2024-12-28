a066e2a297f78c740cb0e62ad5f07c6baf7d5ff9
SJMiguel8 <bonilla.miguel.e@gmail.com>
2024-12-27 22:35:56 -0500

    deployment

--- server.py
+++ server.py
@@ -1,24 +1,29 @@
 from flask import Flask, request, jsonify
-from emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route('/emotion_detection', methods=['POST'])
+@app.route('/emotionDetector', methods=['GET', 'POST'])
 def detect_emotion():
-    data = request.get_json()
-    text_to_analyze = data.get('text')
-    
-    if not text_to_analyze:
-        return jsonify({"error": "Invalid input! Try again."}), 400
-    
-    result = emotion_detector(text_to_analyze)
-    
-    if result['label'] is None:
-        return jsonify({"error": "Invalid input! Try again."}), 400
+    if request.method == 'POST':
+        data = request.get_json()
+        text_to_analyze = data.get('text')
+        
+        if not text_to_analyze:
+            return jsonify({"error": "Invalid input! Try again."}), 400
+        
+        result = emotion_detector(text_to_analyze)
+        response = {
+            "anger": result.get('anger', 0),
+            "disgust": result.get('disgust', 0),
+            "fear": result.get('fear', 0),
+            "joy": result.get('joy', 0),
+            "sadness": result.get('sadness', 0),
+            "dominant_emotion": result.get('dominant_emotion', '')
+        }
+        return jsonify(response), 200
     else:
-        return jsonify({
-            "message": "The given text has been identified as {} with a score of {}.".format(result['label'], result['score'])
-        })
+        return jsonify({"message": "Use POST method to analyze emotions."}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
